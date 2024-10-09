5aeb185a4c7d8d7e8a474c0a0f2ebe88fd1aeda0
doguto <doguto110@gmail.com>
2024-10-09 09:02:40 +0900

    [feat] Model main.

---
+++ Assets/Project/Model/Scripts/Main/OthelloModel.cs
@@ -0,0 +1,83 @@
+using Codice.Client.BaseCommands.Download;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UniRx;
+using System;
+
+public class OthelloModel
+{
+    readonly OthelloColor _white = OthelloColor.white;
+    readonly OthelloColor _black = OthelloColor.black;
+
+    readonly OthelloColor _firstTurn = OthelloColor.black;
+    OthelloColor _currentTurn = OthelloColor.None;
+
+    Board _board;
+
+    Subject<SetOthelloMessage> _setOthelloMessage = new Subject<SetOthelloMessage>();
+    Subject<ChangeColorMessage> _changeColorMessage = new Subject<ChangeColorMessage>();
+
+    public IObservable<SetOthelloMessage> SetOthelloMessage => _setOthelloMessage;
+    public IObservable<ChangeColorMessage> ChangeColorMessage => _changeColorMessage;
+
+    public OthelloModel()
+    {
+        _board = new Board();
+
+        SetOthello(new Vector2Int(3, 3), _black);
+        SetOthello(new Vector2Int(3, 4), _white);
+        SetOthello(new Vector2Int(4, 3), _white);
+        SetOthello(new Vector2Int(4, 4), _black);
+
+        _board.GetInitialized();
+        _currentTurn = _firstTurn;
+    }
+
+    public void SetOthello(Vector2Int position)
+    {
+        SetOthello(position, _currentTurn);
+    }
+
+    public void SetOthello(Vector2Int position, OthelloColor color)
+    {
+        if (_board.HasOthello(position)) return;
+
+        var candidates = _board.GetPuttableGrid(color);
+        if (!candidates.Contains(position)) return;
+
+        var message = new SetOthelloMessage(position, color);
+        _board.SetOthello(position, color);
+        _setOthelloMessage.OnNext(message);
+
+        List<Vector2Int> changeOhtellos = new List<Vector2Int>();
+        changeOhtellos = _board.GetChangeOthello(position, color);
+        foreach (Vector2Int pos in changeOhtellos)
+        {
+            ChangeOthelloColor(pos);
+        }
+
+        ChangeTurn();
+    }
+
+    internal void ChangeOthelloColor(Vector2Int position)
+    {
+        if (_board.HasOthello(position)) return;
+
+        var message = new ChangeColorMessage(position);
+        _board.ChangeColor(position);
+        _changeColorMessage.OnNext(message);
+    }
+
+    void ChangeTurn()
+    {
+        if (_currentTurn == _white)
+        {
+            _currentTurn = _black;  
+        }
+        else if (_currentTurn == _black)
+        {
+            _currentTurn = _white;
+        }
+    }
+}
