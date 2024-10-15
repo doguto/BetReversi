e8b99940639b2a59bdac6d8460a1b4e348bfe68c
doguto <doguto110@gmail.com>
2024-10-15 11:52:00 +0900

    [Feat] ReversiView. can init reversi

--- Assets/Project/View/Scripts/Reversi/ReversiView.cs
+++ Assets/Project/View/Scripts/Reversi/ReversiView.cs
@@ -1,8 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
 
 public class ReversiView : MonoBehaviour
 {
-    [SerializeField] GameObject _othello;
+    [SerializeField] GameObject _othelloPrefab; // Up is black.
+    ReversiPresenter _presenter;
+
+    readonly Vector3 _whiteInit = new Vector3(0, 180, 0);
+
+    private void Start()
+    {
+        _presenter = new ReversiPresenter();
+        _presenter.othelloPresenters.ObserveAdd().Subscribe((presenter) =>
+        {
+            var othello = Instantiate(_othelloPrefab);
+            othello.GetComponent<OthelloView>().Init(presenter.Value);
+            if (presenter.Value.color.Value == OthelloColor.white)
+            {
+                othello.transform.localEulerAngles = _whiteInit;
+            }
+        });
+    }
+
 }
