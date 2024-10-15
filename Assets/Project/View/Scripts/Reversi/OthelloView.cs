887cf86d3f5c33edf8d7440cef66e9d9dbb685c6
doguto <doguto110@gmail.com>
2024-10-15 11:53:09 +0900

    [Feat] OthelloView. init, change color

--- Assets/Project/View/Scripts/Reversi/OthelloView.cs
+++ Assets/Project/View/Scripts/Reversi/OthelloView.cs
@@ -1,8 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UniRx;
+using DG.Tweening;
 
 public class OthelloView : MonoBehaviour
 {
+    OthelloPresenter _presenter;
+    Transform _transform;
 
+    readonly Vector3 _rotateVector = new Vector3(0, 180, 0);
+    readonly float _rotateTime;
+
+
+    internal void Init(OthelloPresenter presenter)
+    {
+        _transform = transform;
+        _transform.position = new Vector3 (presenter.position.x, presenter.position.y, 0);
+
+        _presenter = presenter;
+        _presenter.color.Subscribe((color) =>
+        {
+            _transform.DOLocalRotate(_rotateVector, _rotateTime);
+        });
+    }
 }
