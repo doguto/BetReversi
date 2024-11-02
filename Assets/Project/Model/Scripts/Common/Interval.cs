fba7bd11f5ea97019477d20f9d2f12aa729591c9
doguto <doguto110@gmail.com>
2024-11-03 00:31:31 +0900

    [Feat] static class, Interval having Deray()

---
+++ Assets/Project/Model/Scripts/Common/Interval.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+internal static class Interval
+{
+    internal static async Task Deray(int derayTime)
+    {
+        await Task.Delay(1000);
+    }
+}
